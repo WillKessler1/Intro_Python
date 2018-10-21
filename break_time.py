385c49e13acadeee190c47fb9158e860e6b58916
Kessler <wkessler@ufl.edu>
2018-10-21 15:09:26 -0400

    Add repeating breaks

--- break_time.py
+++ break_time.py
@@ -1,6 +1,10 @@
 import webbrowser
 import time
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=oQDLAoPs-as")
+total_breaks = 4
+breaks_count = 0
 
+while (breaks_count<=total_breaks) :
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=oQDLAoPs-as")
+    breaks_count= breaks_count+1
