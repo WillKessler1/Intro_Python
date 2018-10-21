ba43dc00fe1c8ab3bc2080922b0b407077f75264
Kessler <wkessler@ufl.edu>
2018-10-21 15:32:44 -0400

    update timing to 2 hours

--- break_time.py
+++ break_time.py
@@ -4,7 +4,9 @@ import time
 total_breaks = 4
 breaks_count = 0
 
+print("This program started on "+time.ctime())
 while (breaks_count<=total_breaks) :
-    time.sleep(10)
+    time.sleep(2*3600)
     webbrowser.open("https://www.youtube.com/watch?v=oQDLAoPs-as")
     breaks_count= breaks_count+1
+    
